c0f052edc5330c0adf28475e18c7facc091fa269
boribo <boriboboem@gmail.com>
2025-08-26 15:44:32 +0700

    Completed

--- backend/server.js
+++ backend/server.js
@@ -339,7 +339,7 @@ app.delete("/api/delete-book/:id", async (req, res) => {
 if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("/*", (req, res) => {
+app.get("/*splat", (req, res) => {
   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 });
 
